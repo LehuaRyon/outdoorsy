574688e6c3245ac7f35f1644257d8bb86ca2f637
Jasmine Ryon <jryon12@priorypride.net>
2022-01-25 20:52:25 -0800

    col_sep only accepts one value as a string

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,21 +1,27 @@
 class Customer < ApplicationRecord
     # set of headers, column names, will be ignored
     def self.import(file)
-        # CSV.foreach(file.path, headers: true) do |row|
-        CSV.foreach(file.path) do |row|
-            # Customer.create! row.to_hash
-            # # if does not work, throw error
+        # File.new("/tmp/big.csv").grep(/(^,|,(,|%))/).each do |row_string|
+        #     CSV.parse(row_string) do |row|
+        #       puts row[1]
+        #     end
+        #   end
 
-            Customer.create({
-                first_name: row[0],
-                last_name: row[1],
-                email: row[2],
-                vehicle_type: row[3],
-                vehicle_name: row[4],
-                vehicle_length: row[5]
-            })
+        # col_sep only accepts one value as a string
+        byebug
+        CSV.foreach(file.path, col_sep: "|") do |row|
+            # CSV.foreach(file.path) do |row|
+                # Customer.create! row.to_hash
+                # if does not work, throw error
+
+                Customer.create({
+                    first_name: row[0],
+                    last_name: row[1],
+                    email: row[2],
+                    vehicle_type: row[3],
+                    vehicle_name: row[4],
+                    vehicle_length: row[5]
+                })
         end
     end
-    
 end
-
