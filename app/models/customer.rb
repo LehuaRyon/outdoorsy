7ab5d99f25f8736bd83af1ca3cde1c5e199ae495
Jasmine Ryon <jryon12@priorypride.net>
2022-01-25 17:12:16 -0800

    custom method in Customer model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,8 @@
 class Customer < ApplicationRecord
+    # set of headers, column names, will be ignored
+    CSV.foreach(file.path, headers: true) do |row|
+        Customer.create! row.to_hash
+        # if does not work, throw error
+    end
+    
 end
