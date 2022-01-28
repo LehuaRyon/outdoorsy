73fc1603d147b30933dc19b510ba348c8b57c49d
Jasmine Ryon <jryon12@priorypride.net>
2022-01-28 10:59:20 -0800

    change varible name for clarity

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -9,25 +9,25 @@ class Customer < ApplicationRecord
         Customer.all.sort_by { |customer| [customer[:first_name], customer[:last_name]] }
     end
 
-# This Method was only capable of accepting only one separator value
-# col_sep only accepts one value as a string
-    # def self.import(file)
-    #     CSV.foreach(file_path, headers: false, col_sep: "|") do |row|
-    #             Customer.find_or_create_by({
-    #                 first_name: row[0],
-    #                 last_name: row[1],
-    #                 email: row[2],
-    #                 vehicle_type: row[3],
-    #                 vehicle_name: row[4],
-    #                 vehicle_length: row[5]
-    #             })
-    #     end
-    # end
+    # This Method was only capable of accepting only one separator value
+    # col_sep only accepts one value as a string
+        # def self.import(file)
+        #     CSV.foreach(file_path, headers: false, col_sep: "|") do |row|
+        #             Customer.find_or_create_by({
+        #                 first_name: row[0],
+        #                 last_name: row[1],
+        #                 email: row[2],
+        #                 vehicle_type: row[3],
+        #                 vehicle_name: row[4],
+        #                 vehicle_length: row[5]
+        #             })
+        #     end
+        # end
 
     def self.import(file)
         separators = ["|", ","]
         csv_first_input = File.open(file).first
-        CSV.foreach(file, headers: false, col_sep:  separators.detect{ |separator| csv.include?(separator)}) do |row|
+        CSV.foreach(file, headers: false, col_sep:  separators.detect{ |separator| csv_first_input.include?(separator)}) do |row|
                 # Customer.create!({
                 Customer.find_or_create_by({
                     first_name: row[0],
