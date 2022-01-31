735dc08f06b9ee8cfd818d9676dc56ef098c5cc2
Jasmine Ryon <jryon12@priorypride.net>
2022-01-31 08:44:39 -0800

    add info to javascript.js

--- app/assets/stylesheets/application.js
+++ app/assets/stylesheets/application.js
@@ -1,5 +1,10 @@
 require("jquery")
 // require("jquery_ujs")
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap
+//= require turbolinks
+//= require_tree .
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,8 @@
-require("jquery")
-// require("jquery_ujs")
-
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
+
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
